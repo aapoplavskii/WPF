a6a193d9769ee267c827df4755edc30ae4d411a8
Andrey Poplavskii <andris838@gmail.com>
2021-10-29 18:46:50 +0300

    Доработка класса employee

--- HomeWork/WpfHomeWork/Employee.cs
+++ HomeWork/WpfHomeWork/Employee.cs
@@ -6,5 +6,14 @@ namespace WpfHomeWork
     {
         public string Name { get; set; }
         public int ZP { get; set; }
+
+        public Employee LeftNode { get; set; }
+
+        public Employee RightNode { get; set; }
+
+        public override string ToString()
+        {
+            return Name + " - " + ZP.ToString();
+        }
     }
 }
